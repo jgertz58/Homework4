3f6451fd7eb8b89d1e974fdc8ffb66aebfd0cee1
jgertz58 <jg625686@wne.edu>
2026-04-30 00:41:24 +0000

    test: gameStart

---
+++ src/test/java/com/example/project/TicTacToeTest.java
@@ -0,0 +1,18 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest{
+
+    @Test
+    void gameStart(){
+        TicTacToe game = new TicTacToe();
+
+        assertEquals('X', game.getCurrentPlayer());
+    }
+}
