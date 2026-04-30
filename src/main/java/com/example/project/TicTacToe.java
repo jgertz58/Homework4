17ebcbda15d600e9bfb41a6a3dc257ae69304dd3
jgertz58 <jg625686@wne.edu>
2026-04-30 01:24:32 +0000

    clean takenSquare

--- src/main/java/com/example/project/TicTacToe.java
+++ src/main/java/com/example/project/TicTacToe.java
@@ -9,19 +9,25 @@ public class TicTacToe{
         return currentPlayer;
     }
 
-    /*public void play(int row, int col){
-        if (board[row][col] != '\0') {
+    public void play(int row, int col){
+        if (isTaken(row, col)) {
             throw new IllegalArgumentException("Square is Taken");
         }
 
         board[row][col] = currentPlayer;
+        switchPlayer();
+    }
+
+    private boolean isTaken(int row, int col){
+        return board[row][col] != '\0';
+    }
+
+    private void switchPlayer(){
         if (currentPlayer == 'X'){
             currentPlayer = 'O';
         } else {
             currentPlayer = 'X';
         }
-    }*/
-
-    
+    }
 
 }
